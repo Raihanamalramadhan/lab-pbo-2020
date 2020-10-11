d4ebc9fc79312ccf2d09446339b98490150c1b4c
Raihan Amal Ramadhan <rayhanamalr@gmail.com>
2020-10-11 23:00:04 +0700

    Membuat Kode Pertemuan 1

--- Pertemuan 1/prak3lat7.java
+++ Pertemuan 1/prak3lat7.java
@@ -6,11 +6,10 @@ public class prak3Lat7 {
  public static void main(String args[]) {
 
  	String name = "James";
-	 // following will return true since name is type of String
- 
+// following will return true since name is type of String
  	boolean result = name instanceof String;
  	System.out.println( result );
- 	}
+ }
 }
 
 /*
